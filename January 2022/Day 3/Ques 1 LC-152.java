758f73853e3e48a878015264a18db50b5c4ec6f9
simarpreetsingh-019 <simarpreetsingh.019@gmail.com>
2022-01-26 03:09:43 +0530

    Day 3 updated
    1 question, leetcode 152 and some intro to graphs

---
+++ January 2022/Day 3/Ques 1 LC-152.java
@@ -0,0 +1,18 @@
+// Leetcode 152 Maximum product subarray
+//Language : Java
+// time : 1ms
+
+class Solution {
+    public int maxProduct(int[] nums) {
+         int size = nums.length;
+        int max = nums[0], min=nums[0], maxSum = nums[0];
+        for(int i =1; i<size; i++){
+            int currMax = Math.max(Math.max(max*nums[i], min*nums[i]), nums[i]);
+            int currMin = Math.min(Math.min(max*nums[i], min*nums[i]), nums[i]);
+            max = currMax;
+            min = currMin;
+            maxSum = Math.max(currMax, maxSum);
+        }
+        return maxSum;
+    }
+}
---
+++ January 2022/Day 3/Readme.md
@@ -0,0 +1,5 @@
+# Day 3: 22/01/2022
+
+| Que No. | From | Description |
+| --- | --- | --- |
+| 1 | LeetCode | [152. Maximum Product Subarray](https://leetcode.com/problems/maximum-product-subarray/) |
