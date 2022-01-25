a85270828467835c02e7374f0522ec5f31b475d0
simarpreetsingh-019 <simarpreetsingh.019@gmail.com>
2022-01-26 03:25:34 +0530

    Day 4

---
+++ January 2022/Day 4/Ques 1 LC-203.java
@@ -0,0 +1,32 @@
+// Leetcode 203 Remove LinkedList Element
+// Language : Java
+// time : 0ms
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int target) {
+         while (head!=null && head.val==target){
+            head=head.next;
+        }
+        
+        ListNode temp = head;
+        
+        while(temp!=null && temp.next!=null){
+            if(temp.next.val==target){
+                temp.next=temp.next.next;
+            } else {
+                temp=temp.next;
+            }
+        }
+        return head;
+    }
+}
---
+++ January 2022/Day 4/Readme.md
@@ -0,0 +1,6 @@
+# Day 4: 23/01/2022
+
+| Que No. | From | Description |
+| --- | --- | --- |
+| 1 | LeetCode | [203. Remove Linkedlist Element](https://leetcode.com/problems/remove-linked-list-elements/) |
+| 2 | `Self`| Started learning Graphs |
